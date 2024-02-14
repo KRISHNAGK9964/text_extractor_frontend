60e00cca7ceee597b14f73c9fc769c3d0cecc81c
KRISHNA K <kkrishna22572@gmail.com>
2024-02-14 17:33:37 +0530

    [feat] : upload and extract text done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,77 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
+  const imageMimeType = /image\/(png|jpg|jpeg)/i;
+  const [file, setFile] = useState(null);
+  const [fileDataURL, setFileDataURL] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [data, setData] = useState(null);
 
-  const handleChange = (event) => {
-    console.log(event.target.);
-  }
+  const handleInputChange = (e) => {
+    const file = e.target.files[0];
+    if (!file.type.match(imageMimeType)) {
+      alert("Image mime type is not valid");
+      return;
+    }
+    setFile(file);
+  };
+  useEffect(() => {
+    let fileReader,
+      isCancel = false;
+    if (file) {
+      fileReader = new FileReader();
+      fileReader.onload = (e) => {
+        const { result } = e.target;
+        if (result && !isCancel) {
+          setFileDataURL(result);
+          console.log(result);
+        }
+      };
+      fileReader.readAsDataURL(file);
+    }
+    return () => {
+      isCancel = true;
+      if (fileReader && fileReader.readyState === 1) {
+        fileReader.abort();
+      }
+    };
+  }, [file]);
+
+  const uploadHandler = async (event) => {
+    if(!fileDataURL){
+      return;
+    }
+    try {
+      setLoading(true);
+      const res = await fetch("http://localhost:5000/api/upload-image", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Accept: "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify({
+          base64: fileDataURL,
+        }),
+      });
+      if (res.ok) {
+        const data = await res.json();
+        console.log(data);
+        setData(data.newImageDoc);
+      }
+    } catch (error) {
+      setError(error.message);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
-    <section className="container w-full mx-auto items-center py-32">
-      <div className="max-w-sm mx-auto bg-white rounded-lg shadow-md overflow-hidden items-center">
+    <section className="border w-full min-h-screen items-center flex gap-10 justify-center">
+      <div className="max-w-sm bg-white rounded-lg shadow-md overflow-hidden items-center">
         <div className="px-4 py-6">
           <div
             id="image-preview"
@@ -21,20 +82,20 @@ function App() {
               type="file"
               className="hidden"
               accept="image/*"
-              onChange={handleChange}
+              onChange={handleInputChange}
             />
-            <label for="upload" className="cursor-pointer">
+            <label htmlFor="upload" className="cursor-pointer">
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 fill="none"
                 viewBox="0 0 24 24"
-                stroke-width="1.5"
+                strokeWidth="1.5"
                 stroke="currentColor"
                 className="w-8 h-8 text-gray-700 mx-auto mb-4"
               >
                 <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
                   d="M3 16.5v2.25A2.25 2.25 0 005.25 21h13.5A2.25 2.25 0 0021 18.75V16.5m-13.5-9L12 3m0 0l4.5 4.5M12 3v13.5"
                 />
               </svg>
@@ -54,16 +115,22 @@ function App() {
                 className="text-gray-500 bg-gray-200 z-50"
               ></span>
             </label>
+            {file && <img src={fileDataURL} alt="image" />}
           </div>
           <div className="flex items-center justify-center">
             <div className="w-full">
               <label className="w-full text-white bg-[#050708] hover:bg-[#050708]/90 focus:ring-4 focus:outline-none focus:ring-[#050708]/50 font-medium rounded-lg text-sm px-5 py-2.5 flex items-center justify-center mr-2 mb-2 cursor-pointer">
-                <span className="text-center ml-2">Upload</span>
+                <button disabled={!fileDataURL} onClick={uploadHandler} className="text-center ml-2">
+                  Extract text
+                </button>
               </label>
             </div>
           </div>
         </div>
       </div>
+      {data && (
+        <p className="flex-1">{data.textContent}</p>
+      )}
     </section>
   );
 }
